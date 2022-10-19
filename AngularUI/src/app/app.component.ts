b78049dfaf10544aafcf46ecb0a255f216ce0388
imtiyaz-hirani <i.hirani@techskillsit.com>
2022-10-19 09:57:48 +0530

    exercise 1: calc

--- AngularUI/src/app/app.component.ts
+++ AngularUI/src/app/app.component.ts
@@ -1,10 +1,38 @@
 import { Component } from '@angular/core';
 
-@Component({
+@Component({ //<-- decorator
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'AngularUI';
+  x:number=20;
+  y:number=10;
+  result:number;
+
+  calc(op:string){
+    console.log('calc called... ' + op);
+    switch(op){
+      case 'add':
+        this.result = this.x + this.y;
+        break;
+      case 'sub':
+        this.result = this.x - this.y;
+        break;
+      case 'mul':
+        this.result = this.x * this.y;
+        break;
+      case 'div':
+        this.result = this.x / this.y;
+        break;
+    }
+  }
 }
+/*
+  number: 10, 10.4
+  string
+  boolean
+  any
+
+  String Interpolation Operator: {{}}
+*/
