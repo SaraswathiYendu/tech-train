33c4dd6a629e58dcf4ac23e218a14abd2bcc76bb
imtiyaz-hirani <i.hirani@techskillsit.com>
2022-10-27 12:58:06 +0530

    signup page added

--- ecs-ui/src/app/auth/sign-up/sign-up.component.ts
+++ ecs-ui/src/app/auth/sign-up/sign-up.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormGroup,FormControl,Validators} from '@angular/forms';
 @Component({
   selector: 'app-sign-up',
   templateUrl: './sign-up.component.html',
@@ -7,9 +7,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignUpComponent implements OnInit {
 
+  signupForm: FormGroup;
+  msg:string;
   constructor() { }
 
   ngOnInit(): void {
+    this.signupForm = new FormGroup({
+      name : new FormControl('',[Validators.required, Validators.pattern(/^[a-zA-Z ]+$/)]),
+      email : new FormControl('',[Validators.required,Validators.email]),
+      password : new FormControl('',[Validators.required, Validators.minLength(4)]),
+      jobTitle: new FormControl('', Validators.required),
+      managerId: new FormControl('',Validators.required)
+    });
+  }
+
+  onFormSubmit(){
+
   }
 
 }
